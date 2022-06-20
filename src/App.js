503ff53a1c7bcee5662b710f3adc7a72436959c2
unknown <rockydasgfx@gmail.com>
2022-06-21 03:17:19 +0600

    create page component and navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
+import Reviews from "./Pages/Reviews/Reviews";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container">
+          <NavLink className="navbar-brand fw-bold text-secondary" to="/">
+            ESST Antivirus
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="home"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " aria-current="page" to="reviews">
+                  Reviews
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link "
+                  aria-current="page"
+                  to="dashboard"
+                >
+                  Dashboard
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " aria-current="page" to="blogs">
+                  Blogs
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " aria-current="page" to="about">
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
